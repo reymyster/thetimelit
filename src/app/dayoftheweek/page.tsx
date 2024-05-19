7bcfa97e78d4b191d06b1488657f422b759b4db8
Rey Trajano <rey@reytrajano.com>
2024-05-19 08:27:14 -0700

    font and metadata changes

--- src/app/dayoftheweek/page.tsx
+++ src/app/dayoftheweek/page.tsx
@@ -2,7 +2,7 @@ export default function DayOfTheWeekPage() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center">
            <div className="mx-8 lg:max-w-4xl backdrop-blur-sm p-12 lg:p-24 rounded-3xl shadow-xl bg-gray-100/50 border border-white/50 text-gray-800">
-            <blockquote className="text-2xl lg:text-4xl"><p>On the <span className="font-extrabold">Monday</span> morning, so far as I can tell it, nothing happened to disturb the customary quiet of the house.</p><footer className="mt-4"><cite>&quot;The Moonstone&quot;</cite> by Wilkie Collins </footer></blockquote>
+            <blockquote className="text-2xl lg:text-4xl text-balance"><p>On the <span className="font-bold">Monday</span> morning, so far as I can tell it, nothing happened to disturb the customary quiet of the house.</p><footer className="mt-4 text-lg"><cite>&quot;The Moonstone&quot;</cite> by Wilkie Collins </footer></blockquote>
            </div>
         </main>
     )
