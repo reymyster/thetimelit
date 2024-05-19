64073a80dbd0f80ab2c10e31b531fcc7f4c911b3
Rey Trajano <rey@reytrajano.com>
2024-05-19 08:07:31 -0700

    initial day of the week test

---
+++ src/app/dayoftheweek/page.tsx
@@ -0,0 +1,9 @@
+export default function DayOfTheWeekPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center">
+           <div className="mx-8 lg:max-w-4xl backdrop-blur-sm p-12 lg:p-24 rounded-3xl shadow-xl bg-gray-100/50 border border-white/50 text-gray-800">
+            <blockquote className="text-2xl lg:text-4xl"><p>On the <span className="font-extrabold">Monday</span> morning, so far as I can tell it, nothing happened to disturb the customary quiet of the house.</p><footer className="mt-4"><cite>"The Moonstone"</cite> by Wilkie Collins </footer></blockquote>
+           </div>
+        </main>
+    )
+}
