f4ee270375b046f69a49ba27ea81af725d724c6a
Rey Trajano <rey@reytrajano.com>
2024-07-04 08:31:16 -0700

    fix linting issue

--- src/app/manage/quotes/columns.tsx
+++ src/app/manage/quotes/columns.tsx
@@ -1,69 +1,23 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
-import Link from "next/link";
 import { type Quote } from "@/dbschema/interfaces";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useDeleteQuoteMutation } from "@/lib/db/admin/hooks";
-import { useToast } from "@/components/ui/use-toast";
+import { DataTableRowActions } from "./data-table-row-actions";
 
 export const columns: ColumnDef<Quote>[] = [
   {
     accessorKey: "text",
     header: "Text",
-    cell: ({ row }) => {
-      const text = row.getValue<string>("text");
-
+    cell: ({
+      row: {
+        original: { text },
+      },
+    }) => {
       return <div className="max-w-lg truncate">{text}</div>;
     },
   },
   {
     id: "actions",
-    cell: ({ row }) => {
-      const id = row.original.id;
-      const deleter = useDeleteQuoteMutation();
-      const { toast } = useToast();
-
-      const confirmDelete = async () => {
-        if (confirm("Really delete this quote?")) {
-          await deleter.mutateAsync(id);
-          toast({
-            variant: "destructive",
-            title: "Quote deleted",
-            description: "This quote has been deleted.",
-          });
-        }
-      };
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="size-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="size-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem asChild>
-              <Link href={`/manage/quote/edit/${id}`} prefetch={false}>
-                Edit
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={confirmDelete}>Delete</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
+    cell: ({ row }) => <DataTableRowActions row={row} />,
   },
 ];
---
+++ src/app/manage/quotes/data-table-row-actions.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { MoreHorizontal } from "lucide-react";
+import Link from "next/link";
+import { Row } from "@tanstack/react-table";
+import { type Quote } from "@/dbschema/interfaces";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useToast } from "@/components/ui/use-toast";
+import { useDeleteQuoteMutation } from "@/lib/db/admin/hooks";
+
+interface DataTableRowActionProps {
+  row: Row<Quote>;
+}
+
+export function DataTableRowActions({
+  row: {
+    original: { id },
+  },
+}: DataTableRowActionProps) {
+  const deleter = useDeleteQuoteMutation();
+  const { toast } = useToast();
+
+  const confirmDelete = async () => {
+    if (confirm("Really delete this quote?")) {
+      await deleter.mutateAsync(id);
+      toast({
+        variant: "destructive",
+        title: "Quote deleted",
+        description: "This quote has been deleted.",
+      });
+    }
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="size-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem asChild>
+          <Link href={`/manage/quote/edit/${id}`} prefetch={false}>
+            Edit
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={confirmDelete}>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
